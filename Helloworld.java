965515d5c06e3950e08b466d3de50c1efc8d3c69
Mr.TomaTo <33390996+vogiahuy7777@users.noreply.github.com>
2018-10-15 09:11:55 +0700

    tdtu branch revision 2

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Helloworld{
 	public static void main (String[] args){
-		System.out.print("Hello Word I'm TDTU");
+		System.out.print("Hello Word This is TDTU");
 	}
 }
 	
